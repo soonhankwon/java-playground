06dfa20916ee0a3c7d5aaea959be8012b9aa6d79
soonable@naver.com <soonable@naver.com>
2023-03-30 14:28:12 +0900

    feat : 연료주입 문제 기본 구조 tdd

---
+++ src/fuel/Car.java
@@ -0,0 +1,9 @@
+package fuel;
+
+public class Car {
+    private int distance;
+
+    public Car(int distance) {
+        this.distance = distance;
+    }
+}
---
+++ src/fuel/RentCompany.java
@@ -0,0 +1,14 @@
+package fuel;
+
+public class RentCompany {
+    public static RentCompany create() {
+        return null;
+    }
+
+    public void addCar(Car Car) {
+    }
+
+    public String generateReport() {
+        return null;
+    }
+}
