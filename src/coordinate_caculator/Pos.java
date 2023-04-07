eede3d02bc28accd5a919373fc505c2c21c4c8c1
soonable@naver.com <soonable@naver.com>
2023-04-07 16:01:46 +0900

    feat : X,Y 좌표 0 미만 입력 불가

--- src/coordinate_caculator/Pos.java
+++ src/coordinate_caculator/Pos.java
@@ -2,18 +2,19 @@ package coordinate_caculator;
 
 public class Pos {
     public static final int MAX_POS = 24;
+    public static final int MIN_POS = 0;
     private int xPos;
     private int yPos;
 
     public Pos(int xPos, int yPos) {
-        if(isPosUpperMaxNumber(xPos, yPos)) {
+        if(isPosInvalidNumber(xPos, yPos)) {
             throw new IllegalArgumentException();
         }
         this.xPos = xPos;
         this.yPos = yPos;
     }
 
-    private boolean isPosUpperMaxNumber(int xPos, int yPos) {
-        return xPos > MAX_POS || yPos > MAX_POS;
+    private boolean isPosInvalidNumber(int xPos, int yPos) {
+        return xPos > MAX_POS || yPos > MAX_POS || xPos < MIN_POS || yPos < MIN_POS;
     }
 }
--- src/test/coordinate_calculator/PosTest.java
+++ src/test/coordinate_calculator/PosTest.java
@@ -1,15 +1,26 @@
 package test.coordinate_calculator;
 
 import coordinate_caculator.Pos;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PosTest {
     @Test
+    @DisplayName("X, Y좌표 모두 최대 24까지만 입력 테스트")
     void maxPosNumber() {
         assertThatThrownBy(() -> {
             Pos pos = new Pos(25,25);
                 }).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("X, Y좌표 모두 0이하 입력 불가 테스트")
+    void minPosNumber() {
+        assertThatThrownBy(() -> {
+            Pos pos = new Pos(-1,1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
