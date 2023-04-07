9aee713afd578bccb384416daa8043ada2287a33
soonable@naver.com <soonable@naver.com>
2023-04-07 16:24:41 +0900

    test : 두 좌표사이 직선거리 계산 테스트

--- src/test/coordinate_calculator/PosTest.java
+++ src/test/coordinate_calculator/PosTest.java
@@ -4,6 +4,9 @@ import coordinate_caculator.Pos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PosTest {
@@ -21,6 +24,24 @@ public class PosTest {
         assertThatThrownBy(() -> {
             Pos pos = new Pos(-1,1);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("두 좌표 사이 거리 계산 테스트")
+    void calculateDistance() {
+        Pos pos1 = new Pos(10,10);
+        Pos pos2 = new Pos(14,15);
+
+        double num1 = Math.pow(pos1.getX() - pos2.getX(), 2);
+        double num2 = Math.pow(pos1.getY() - pos2.getY(), 2);
+
+        DecimalFormat df = new DecimalFormat("0.000000");
+        double distance = Math.sqrt(num1 + num2);
+        String result = df.format(distance);
+
+        assertThat(num1).isEqualTo(16);
+        assertThat(num2).isEqualTo(25);
+        assertThat(result).isEqualTo("6.403124");
     }
 
 }
